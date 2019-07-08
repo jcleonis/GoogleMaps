920e9488a90133cecbd5d9bafcad47534e2237d4
Jonatas Leonis <jcleonis@hotmail.com>
2019-07-07 22:22:42 -0300

    Primeiro Commit

---
+++ GoogleMaps/GoogleMaps/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace GoogleMaps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.lblLongitude = new System.Windows.Forms.Label();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.lblLatitude = new System.Windows.Forms.Label();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnPoligono = new System.Windows.Forms.Button();
+            this.btnRotas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(928, 719);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseDoubleClick);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(946, 176);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(137, 30);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(1119, 176);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(137, 30);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(946, 32);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(310, 22);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(946, 12);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(71, 17);
+            this.lblDescricao.TabIndex = 4;
+            this.lblDescricao.Text = "Descrição";
+            // 
+            // lblLongitude
+            // 
+            this.lblLongitude.AutoSize = true;
+            this.lblLongitude.Location = new System.Drawing.Point(946, 62);
+            this.lblLongitude.Name = "lblLongitude";
+            this.lblLongitude.Size = new System.Drawing.Size(71, 17);
+            this.lblLongitude.TabIndex = 6;
+            this.lblLongitude.Text = "Longitude";
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Location = new System.Drawing.Point(946, 82);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(310, 22);
+            this.txtLongitude.TabIndex = 5;
+            // 
+            // lblLatitude
+            // 
+            this.lblLatitude.AutoSize = true;
+            this.lblLatitude.Location = new System.Drawing.Point(946, 113);
+            this.lblLatitude.Name = "lblLatitude";
+            this.lblLatitude.Size = new System.Drawing.Size(59, 17);
+            this.lblLatitude.TabIndex = 8;
+            this.lblLatitude.Text = "Latitude";
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Location = new System.Drawing.Point(946, 133);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(310, 22);
+            this.txtLatitude.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(946, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 479);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.SelecionarGeoLocalizacao);
+            // 
+            // btnPoligono
+            // 
+            this.btnPoligono.Location = new System.Drawing.Point(946, 216);
+            this.btnPoligono.Name = "btnPoligono";
+            this.btnPoligono.Size = new System.Drawing.Size(137, 30);
+            this.btnPoligono.TabIndex = 10;
+            this.btnPoligono.Text = "Poligono";
+            this.btnPoligono.UseVisualStyleBackColor = true;
+            this.btnPoligono.Click += new System.EventHandler(this.btnPoligono_Click);
+            // 
+            // btnRotas
+            // 
+            this.btnRotas.Location = new System.Drawing.Point(1119, 216);
+            this.btnRotas.Name = "btnRotas";
+            this.btnRotas.Size = new System.Drawing.Size(137, 30);
+            this.btnRotas.TabIndex = 11;
+            this.btnRotas.Text = "Rotas";
+            this.btnRotas.UseVisualStyleBackColor = true;
+            this.btnRotas.Click += new System.EventHandler(this.btnRotas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 743);
+            this.Controls.Add(this.btnRotas);
+            this.Controls.Add(this.btnPoligono);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblLatitude);
+            this.Controls.Add(this.txtLatitude);
+            this.Controls.Add(this.lblLongitude);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Google Maps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Label lblLongitude;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.Label lblLatitude;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPoligono;
+        private System.Windows.Forms.Button btnRotas;
+    }
+}
+
---
+++ GoogleMaps/GoogleMaps/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+
+
+namespace GoogleMaps
+{
+    public partial class Form1 : Form
+    {
+        GMarkerGoogle marker;
+        GMapOverlay markeroverlay;
+        DataTable dt;
+        private int filaselecionada = 0;
+        double latInicial = -23.64339592;
+        double lngInicial = -46.63429248;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Descrição", typeof(string)));
+            dt.Columns.Add(new DataColumn("lat:", typeof(double)));
+            dt.Columns.Add(new DataColumn("long:", typeof(double)));
+
+            dt.Rows.Add("Marcação 1", latInicial, lngInicial);
+            dataGridView1.DataSource = dt;
+
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+
+
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(latInicial, lngInicial);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+            gMapControl1.AutoScroll = true;
+
+            //Marcador
+            markeroverlay = new GMapOverlay("Marcador");
+            marker = new GMarkerGoogle(new PointLatLng(latInicial, lngInicial), GMarkerGoogleType.green);
+            markeroverlay.Markers.Add(marker);
+
+            // tooltip
+            marker.ToolTipMode = MarkerTooltipMode.Always;
+            marker.ToolTipText = string.Format("Marcação: \n Latitud: {0} \n Longitud: {1} ", latInicial, lngInicial);
+
+            gMapControl1.Overlays.Add(markeroverlay);
+        }
+
+        private void SelecionarGeoLocalizacao(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+            if (dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].IsNewRow) return;
+
+            filaselecionada   = e.RowIndex;
+
+            txtDescricao.Text = dataGridView1.Rows[filaselecionada].Cells[0].Value.ToString();
+            txtLatitude.Text  = dataGridView1.Rows[filaselecionada].Cells[1].Value.ToString();
+            txtLongitude.Text = dataGridView1.Rows[filaselecionada].Cells[2].Value.ToString();
+            //if (string.IsNullOrEmpty(txtLatitude.Text) || string.IsNullOrEmpty(txtLongitude.Text))
+            //{
+            //    return;
+            //}
+
+            marker.Position = new PointLatLng(Convert.ToDouble(txtLatitude.Text), Convert.ToDouble(txtLongitude.Text));
+            gMapControl1.Position = marker.Position;
+
+        }
+
+        private void gMapControl1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            double lat = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lat;
+            double lng = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lng;
+
+            txtLatitude.Text = lat.ToString();
+            txtLongitude.Text = lng.ToString();
+
+            marker.Position = new PointLatLng(lat, lng);
+            marker.ToolTipText = string.Format("Marcação: \n Latitud: {0} \n Longitud: {1} ", lat, lng);
+            
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Add(txtDescricao.Text, txtLatitude.Text, txtLongitude.Text);
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(filaselecionada);
+        }
+
+        private void btnPoligono_Click(object sender, EventArgs e)
+        {
+            GMapOverlay Poligono = new GMapOverlay("Polígonos");
+            List<PointLatLng> pontos = new List<PointLatLng>();
+            double lat, lng;
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                lat = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                lng = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                pontos.Add(new PointLatLng(lat, lng));
+            }
+
+            GMapPolygon polygonPontos = new GMapPolygon(pontos, "Polígonos");
+            Poligono.Polygons.Add(polygonPontos);
+            gMapControl1.Overlays.Add(Poligono);
+
+            gMapControl1.Zoom += 1;
+            gMapControl1.Zoom -= 1;
+
+        }
+
+        private void btnRotas_Click(object sender, EventArgs e)
+        {
+            GMapOverlay Rotas = new GMapOverlay("CapaRotas");
+            List<PointLatLng> pontos = new List<PointLatLng>();
+            double lat, lng;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                lat = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                lng = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                pontos.Add(new PointLatLng(lat, lng));
+            }
+
+            GMapRoute rotasPontos = new GMapRoute(pontos, "Rotas");
+            Rotas.Routes.Add(rotasPontos);
+            gMapControl1.Overlays.Add(Rotas);
+
+            gMapControl1.Zoom += 1;
+            gMapControl1.Zoom -= 1;
+        }
+    }
+}
